8f5aafec53625965d0e0e90bfdb13926f7bc7726
Srikanth Talapadi <sri@zoolore.org>
2019-02-11 10:49:17 +0530

    Added scaffold to storyboards

--- lib/storyboards/games/game_list_story.dart
+++ lib/storyboards/games/game_list_story.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:jamaica/models/game_config.dart';
 import 'package:jamaica/widgets/game_list.dart';
@@ -6,73 +7,81 @@ import 'package:storyboard/storyboard.dart';
 class GameListStory extends FullScreenStory {
   @override
   List<Widget> get storyContent => [
-        GameList(
-          games: {
-            'Math Games': [
-              GameConfig((b) => b
-                ..name = 'Match the shape'
-                ..image = 'match_the_shape.png'
-                ..levels = 10),
-              GameConfig((b) => b
-                ..name = 'Domino Math'
-                ..image = 'domino_math.png'
-                ..levels = 10),
-              GameConfig((b) => b
-                ..name = 'Memory match'
-                ..image = 'memory_match.png'
-                ..levels = 10),
-            ],
-            'Reading Games': [
-              GameConfig((b) => b
-                ..name = 'Match the following'
-                ..image = 'match_the_following.png'
-                ..levels = 10),
-              GameConfig((b) => b
-                ..name = 'Alphabet'
-                ..image = 'alphabet.png'
-                ..levels = 10),
-            ],
-            'Writing Games': [
-              GameConfig((b) => b
-                ..name = 'Match the shape'
-                ..image = 'match_the_shape.png'
-                ..levels = 10),
-              GameConfig((b) => b
-                ..name = 'Domino Math'
-                ..image = 'domino_math.png'
-                ..levels = 10),
-              GameConfig((b) => b
-                ..name = 'Memory match'
-                ..image = 'memory_match.png'
-                ..levels = 10),
-              GameConfig((b) => b
-                ..name = 'Match the shape'
-                ..image = 'match_the_shape.png'
-                ..levels = 10),
-              GameConfig((b) => b
-                ..name = 'Domino Math'
-                ..image = 'domino_math.png'
-                ..levels = 10),
-            ],
-          },
+        Scaffold(
+          body: SafeArea(
+            child: GameList(
+              games: {
+                'Math Games': [
+                  GameConfig((b) => b
+                    ..name = 'Match the shape'
+                    ..image = 'match_the_shape.png'
+                    ..levels = 10),
+                  GameConfig((b) => b
+                    ..name = 'Domino Math'
+                    ..image = 'domino_math.png'
+                    ..levels = 10),
+                  GameConfig((b) => b
+                    ..name = 'Memory match'
+                    ..image = 'memory_match.png'
+                    ..levels = 10),
+                ],
+                'Reading Games': [
+                  GameConfig((b) => b
+                    ..name = 'Match the following'
+                    ..image = 'match_the_following.png'
+                    ..levels = 10),
+                  GameConfig((b) => b
+                    ..name = 'Alphabet'
+                    ..image = 'alphabet.png'
+                    ..levels = 10),
+                ],
+                'Writing Games': [
+                  GameConfig((b) => b
+                    ..name = 'Match the shape'
+                    ..image = 'match_the_shape.png'
+                    ..levels = 10),
+                  GameConfig((b) => b
+                    ..name = 'Domino Math'
+                    ..image = 'domino_math.png'
+                    ..levels = 10),
+                  GameConfig((b) => b
+                    ..name = 'Memory match'
+                    ..image = 'memory_match.png'
+                    ..levels = 10),
+                  GameConfig((b) => b
+                    ..name = 'Match the shape'
+                    ..image = 'match_the_shape.png'
+                    ..levels = 10),
+                  GameConfig((b) => b
+                    ..name = 'Domino Math'
+                    ..image = 'domino_math.png'
+                    ..levels = 10),
+                ],
+              },
+            ),
+          ),
         ),
-        GameList(
-          games: {
-            'Math Games': [
-              GameConfig((b) => b
-                ..name = 'Match the shape'
-                ..image = 'match_the_shape.png'
-                ..levels = 10),
-              GameConfig((b) => b
-                ..name = 'Domino Math'
-                ..image = 'domino_math.png'
-                ..levels = 10),
-              GameConfig((b) => b
-                ..name = 'Memory match'
-                ..image = 'memory_match.png'
-                ..levels = 10),
-            ],
-          },
+        Scaffold(
+          body: SafeArea(
+            child: GameList(
+              games: {
+                'Math Games': [
+                  GameConfig((b) => b
+                    ..name = 'Match the shape'
+                    ..image = 'match_the_shape.png'
+                    ..levels = 10),
+                  GameConfig((b) => b
+                    ..name = 'Domino Math'
+                    ..image = 'domino_math.png'
+                    ..levels = 10),
+                  GameConfig((b) => b
+                    ..name = 'Memory match'
+                    ..image = 'memory_match.png'
+                    ..levels = 10),
+                ],
+              },
+            ),
+          ),
         )
       ];
 }
--- lib/storyboards/games/sequence_the_number_game_story.dart
+++ lib/storyboards/games/sequence_the_number_game_story.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:jamaica/games/sequence_the_number_game.dart';
 import 'package:storyboard/storyboard.dart';
@@ -5,10 +6,14 @@ import 'package:storyboard/storyboard.dart';
 class SequenceTheNumberGameStory extends FullScreenStory {
   @override
   List<Widget> get storyContent => [
-        SequenceTheNumberGame(
-          sequence: [1, 2, 3, 4],
-          choices: [3, 5, 6],
-          blankPosition: 2,
+        Scaffold(
+          body: SafeArea(
+            child: SequenceTheNumberGame(
+              sequence: [1, 2, 3, 4],
+              choices: [3, 5, 6],
+              blankPosition: 2,
+            ),
+          ),
         )
       ];
 }
