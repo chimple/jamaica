2431973335c51409cf058c182935d24aa3317102
Srikanth Talapadi <sri@zoolore.org>
2019-02-21 15:48:00 +0530

    Added jumbled_words_game

---
+++ lib/games/jumbled_words_game.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:jamaica/widgets/bento_box.dart';
+import 'package:jamaica/widgets/cute_button.dart';
+
+class _ChoiceDetail {
+  String choice;
+  Reaction reaction;
+  _Type type;
+  bool solved;
+
+  _ChoiceDetail({
+    this.choice,
+    this.type = _Type.choice,
+    this.reaction = Reaction.success,
+    this.solved = false,
+  });
+  @override
+  String toString() =>
+      '_ChoiceDetail(choice: $choice, type: $type, solved: $solved, reaction: $reaction)';
+}
+
+enum _Type { choice, question }
+
+class JumbledWordsGame extends StatefulWidget {
+  final String answer;
+  final List<String> choices;
+
+  const JumbledWordsGame({Key key, this.answer, this.choices})
+      : super(key: key);
+
+  @override
+  _JumbledWordsGameState createState() => _JumbledWordsGameState();
+}
+
+class _JumbledWordsGameState extends State<JumbledWordsGame> {
+  List<_ChoiceDetail> choiceDetails;
+  bool thisSolved = false;
+
+  @override
+  void initState() {
+    super.initState();
+    choiceDetails = widget.choices
+        .map((c) => _ChoiceDetail(choice: c))
+        .toList(growable: false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> frontChildren;
+    if (thisSolved) {
+      frontChildren = choiceDetails
+          .where((c) => c.solved)
+          .map((c) => CuteButton(
+                key: Key(c.choice),
+                child: Center(child: Text(c.choice)),
+              ) as Widget)
+          .toList();
+      frontChildren.add(Center(key: Key('answer'), child: Text(widget.answer)));
+    }
+
+    return BentoBox(
+      dragConfig: DragConfig.draggableBounceBack,
+      frontChildren: frontChildren,
+      qRows: 1,
+      qCols: 1,
+      qChildren: thisSolved
+          ? <Widget>[Container()]
+          : <Widget>[
+              DragTarget<String>(
+                key: Key('answer'),
+                builder: (context, candidateData, rejectedData) =>
+                    Center(child: Text(widget.answer)),
+                onWillAccept: (data) => data == widget.answer,
+                onAccept: (data) => WidgetsBinding.instance
+                    .addPostFrameCallback((_) => setState(() {
+                          choiceDetails
+                              .firstWhere((c) => c.choice == data)
+                              .solved = true;
+                          thisSolved = true;
+                        })),
+              )
+            ],
+      rows: 1,
+      cols: choiceDetails.length,
+      children: choiceDetails
+          .map((c) => c.solved
+              ? Container()
+              : CuteButton(
+                  key: Key(c.choice),
+                  child: Center(child: Text(c.choice)),
+                ))
+          .toList(growable: false),
+    );
+  }
+}
