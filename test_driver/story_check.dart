0c3ebc935cebc5ecd27dcd15a8ab8e539b155ec0
Deon <35517512+deonchimple@users.noreply.github.com>
2019-03-08 11:08:31 +0530

    automation scripts

---
+++ test_driver/story_check.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_driver/driver_extension.dart';
+import 'package:jamaica/main.dart' as app;
+import 'package:flutter/material.dart';
+
+void main() {
+  enableFlutterDriverExtension();
+  app.main();
+}
